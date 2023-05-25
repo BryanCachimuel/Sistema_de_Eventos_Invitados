0ddf70e534f0883eea11971d986bbead2ce7a55c
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-24 22:14:13 -0500

    desarrollando la vista que se ve al momento que se a registrado y realizado inicio de sesión un usuario

--- inicio.php
+++ inicio.php
@@ -1,3 +1,10 @@
+<?php 
+  session_start();
+
+  if(!isset($_SESSION['usuario'])){
+    header("location:inicio_sesion.php");
+  }
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -10,31 +17,20 @@
     <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
   <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="https://placeholder.pics/svg/150x50/888888/EEE/Logo" alt="..." height="36">
+    <a class="navbar-brand" href="inicio.php">
+      Sistema de Eventos e Invitados
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            <?php echo $_SESSION['usuario']; ?>
           </a>
           <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="servidor/inicio_sesion/logout.php">Cerrar Sesión</a></li>
           </ul>
         </li>
       </ul>
@@ -44,9 +40,7 @@
 
 <!-- Page Content -->
 <div class="container">
-  <h1 class="mt-4">Logo Nav by Start Bootstrap</h1>
-  <p>The logo in the navbar is now a default Bootstrap feature in Bootstrap! Make sure to set the height
-    of the logo within the HTML or using CSS. For best results, use an SVG image as your logo.</p>
+  
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
