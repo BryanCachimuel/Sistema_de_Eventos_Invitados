e31e377cee8a5bfcd367ee2b59f31582698554b9
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-24 19:44:06 -0500

    creando el método de inicio_sesion para que el usuario pueda ingresar a su cuenta dentro del sistema

--- clases/Auth.php
+++ clases/Auth.php
@@ -8,8 +8,23 @@
             $conexion = parent::conectar();
             $sql = "INSERT INTO usuarios(usuario,correo,password) VALUES(?,?,?)";
             $query = $conexion->prepare($sql);
-            $query->bind_param('ss',$usuario,$correo,$password);
+            $query->bind_param('sss',$usuario,$correo,$password);
             return $query->execute();
+        }
+
+        public function inicio_sesion($usuario, $password){
+            $conexion = parent::conectar();
+            $passwordExistente = "";
+            $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+            $respuesta = mysqli_query($conexion, $sql);
+            $passwordExistente = mysqli_fetch_array($respuesta)['password'];
+            
+            if(password_verify($password, $passwordExistente)){
+                $_SESSION['usuario'] = $usuaio;
+                return true;
+            }else{
+                return false;
+            }
         }
 
     }
