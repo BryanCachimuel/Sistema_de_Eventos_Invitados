ce1ca1b175e4ca597e8d9d66c52b3f386ebc14b6
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-24 22:08:07 -0500

    reforzando la seguridad de las funciones con try y catch

--- clases/Auth.php
+++ clases/Auth.php
@@ -5,26 +5,34 @@
     class Auth extends Conexion {
 
         public function registrar($usuario, $correo, $password){
-            $conexion = parent::conectar();
-            $sql = "INSERT INTO usuarios(usuario,correo,password) VALUES(?,?,?)";
-            $query = $conexion->prepare($sql);
-            $query->bind_param('sss',$usuario,$correo,$password);
-            return $query->execute();
+            try {
+                $conexion = parent::conectar();
+                $sql = "INSERT INTO usuarios(usuario,correo,password) VALUES(?,?,?)";
+                $query = $conexion->prepare($sql);
+                $query->bind_param('sss',$usuario,$correo,$password);
+                return $query->execute();
+            } catch (Exception $e) {
+                echo "No se pudo registrar el usuario";
+            }
         }
 
         public function inicio_sesion($usuario, $password){
-            $conexion = parent::conectar();
-            $passwordExistente = "";
-            $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
-            $respuesta = mysqli_query($conexion, $sql);
-            $passwordExistente = mysqli_fetch_array($respuesta)['password'];
-            
-            if(password_verify($password, $passwordExistente)){
-                $_SESSION['usuario'] = $usuaio;
-                return true;
-            }else{
-                return false;
-            }
+           try {
+                $conexion = parent::conectar();
+                $passwordExistente = "";
+                $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+                $respuesta = mysqli_query($conexion, $sql);
+                $passwordExistente = mysqli_fetch_array($respuesta)['password'];
+                
+                if(password_verify($password, $passwordExistente)){
+                    $_SESSION['usuario'] = $usuario;
+                    return true;
+                }else{
+                    return false;
+                }
+           } catch (Exception $e) {
+                echo "No se pudo realizar el inicio de sesión";
+           }
         }
 
     }
