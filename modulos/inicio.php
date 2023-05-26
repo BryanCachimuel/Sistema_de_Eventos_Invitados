de77994a59a55e73a59fc4bf954f5b1198f6a5e6
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-25 22:47:07 -0500

    agregando una tarjeta de presentación en el inicio.php

--- modulos/inicio.php
+++ modulos/inicio.php
@@ -5,7 +5,15 @@
 
 <!-- Contenido -->
 <div class="container">
-  
+  <div class="row">
+    <div class="col">
+      <div class="card mt-3">
+        <div class="card-body">
+          Este es el Inicio
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php include "partes/footer.php"; ?>
