a15837868c12093a7a7fdf9f466d2fba0062434b
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-27 22:26:01 -0500

    creando la tabla que contendrá el listado de los eventos, esta tabla ya abarca lo que es el uso de DataTables

---
+++ modulos/eventos/tabla_eventos.php
@@ -0,0 +1,28 @@
+<table class="table table-sm table-hover" id="tabla_eventos_load">
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Hora Inicio</th>
+            <th>Hora Fin</th>
+            <th>Fecha</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+<script>
+    $(document).ready(function(){
+        $('#tabla_eventos_load').DataTable();
+    });
+</script>
