e0d4594567715f535edc29c6499a3a3660fa87a5
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-27 20:34:01 -0500

    creando los archivos para la vistas de eventos y listas de invitados

---
+++ modulos/eventos.php
@@ -0,0 +1,23 @@
+<?php 
+  include "partes/header.php"; 
+  include "partes/menu.php";
+?>
+
+<!-- Contenido -->
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card mt-3">
+        <div class="card-body">
+          Listado de Eventos <br>
+          <a href="" class="btn btn-purple">Evento Nuevo</a>
+          <hr>
+          <table id="tablaEventos"></table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include "partes/footer.php"; ?>
+    
---
+++ modulos/listados.php
@@ -0,0 +1,20 @@
+<?php 
+  include "partes/header.php"; 
+  include "partes/menu.php";
+?>
+
+<!-- Contenido -->
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card mt-3">
+        <div class="card-body">
+          Listado de Invitados
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php include "partes/footer.php"; ?>
+    
