a6d04ac00df724b9d6d396329942253c433888de
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-28 21:35:39 -0500

    creando el archivo para realizar la impresión de los listados

---
+++ modulos/imprimir_listado.php
@@ -0,0 +1,16 @@
+<?php 
+  include "partes/header.php"; 
+?>
+
+<!-- Contenido -->
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="card mt-3">
+        <div class="card-body">
+          Imprimir el Listado
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
