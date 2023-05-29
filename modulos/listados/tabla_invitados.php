bc129e39c796d4a5a02054abc40acf227131e91e
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-28 20:03:27 -0500

    estructurando la tabla en la cual se va a ver los registros realizados de la lista de invitados

---
+++ modulos/listados/tabla_invitados.php
@@ -0,0 +1,26 @@
+<table class="table table-sm table-hover" id="tabla_invitados_load">
+    <thead>
+        <tr>
+            <th>Invitado</th>
+            <th>Evento</th>
+            <th>Fecha</th>
+            <th>Editar</th>
+            <th>Eliminar</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </tbody>
+</table>
+
+<script>
+    $(document).ready(function(){
+        $('#tabla_invitados_load').DataTable();
+    });
+</script>
