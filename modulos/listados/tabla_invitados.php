5100e255e4bb11fc9549a3f9f84590e5e40a1dfd
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-29 19:42:50 -0500

    adecuando el script php para poder mostrar en la tabla todos los registros realizados para invitados

--- modulos/listados/tabla_invitados.php
+++ modulos/listados/tabla_invitados.php
@@ -1,7 +1,13 @@
+<?php
+    include "../../clases/Invitados.php";
+    $Invitados = new Invitados();
+    $items =$Invitados->mostrarInvitados();
+?>
 <table class="table table-sm table-hover" id="tabla_invitados_load">
     <thead>
         <tr>
             <th>Invitado</th>
+            <th>Email</th>
             <th>Evento</th>
             <th>Fecha</th>
             <th>Editar</th>
@@ -9,10 +15,12 @@
         </tr>
     </thead>
     <tbody>
+        <?php foreach($items as $inv) : ?>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td><?php echo $inv['nombreInvitado'] ?></td>
+            <td><?php echo $inv['emailInvitado'] ?></td>
+            <td><?php echo $inv['nombreEvento'] ?></td>
+            <td><?php echo $inv['fechaEvento'] ?></td>
             <td>
                 <span class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modal_agregar_invitado">
                     <i class="fa-solid fa-user-pen"></i>
@@ -24,6 +32,7 @@
                 </span>
             </td>
         </tr>
+        <?php endforeach; ?>
     </tbody>
 </table>
 
