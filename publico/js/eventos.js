27b32b7bfdecddb1a0e22fa7f5b6087a8566100e
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-30 20:54:19 -0500

    controlando el funcionamiento mediante js del registro de eventos y realizando el chequeo de los registros mediante sweetalert2

--- publico/js/eventos.js
+++ publico/js/eventos.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
     $('#tablaEventos').load('eventos/tabla_eventos.php');
 });
+
+function agregarEvento() {
+    $.ajax({
+        type:"POST",
+        data:$('#frmAgregarEvento').serialize(),
+        url:"../servidor/eventos/agregar.php",
+        success:function(respuesta){
+            if(respuesta == 1){
+                $('#tablaEventos').load('eventos/tabla_eventos.php');
+                $('#frmAgregarEvento')[0].reset();
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Evento Agregado con Exito',
+                    showConfirmButton: false,
+                    timer: 1700
+                  })
+            }else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'El Evento no se a Podido Agregr' + respuesta,
+                    showConfirmButton: false,
+                    timer: 1700
+                  })
+            }
+        }
+    });
+    return false;
+}
