f7b5a1caae847b31515b49d1b5e01608fd127040
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-30 20:51:41 -0500

    creando la función agregarEvento la cual permitirá realizar los registros de eventos en la base de datos

--- clases/Eventos.php
+++ clases/Eventos.php
@@ -11,10 +11,30 @@
                 $respuesta = mysqli_query($conexion, $sql);
                 return mysqli_fetch_all($respuesta, MYSQLI_ASSOC);
             } catch (Exception $e) {
-                echo "No se a podido mostrar la lista de Eventos";
+                echo "No se a podido mostrar la lista de Eventos: ".$e;
             }
         }
 
+        public function agregarEvento($data){
+            try {
+                $conexion = Conexion::conectar();
+                $sql = "INSERT INTO eventos(id_usuario,
+                                            evento_nombre,
+                                            hora_inicio,
+                                            hora_fin,
+                                            fecha)
+                        VALUES(?,?,?,?,?)";
+                $query = $conexion->prepare($sql);
+                $query->bind_param('issss', $data['id_usuario'],
+                                            $data['evento_nombre'],
+                                            $data['hora_inicio'],
+                                            $data['hora_fin'],
+                                            $data['fecha']);
+                return $query->execute();
+            } catch (Exception $e) {
+                echo "No se a podido agregar el evento nuevo: ".$e;
+            }
+        }
     }
 
 ?>
