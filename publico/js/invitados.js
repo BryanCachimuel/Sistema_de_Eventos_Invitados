af2d2dd3774f32f72e436f0b17491f7981736062
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-31 22:00:19 -0500

    realizando el proceso con javascript para agregar un nuevo invitado

--- publico/js/invitados.js
+++ publico/js/invitados.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
     $('#tablaInvitados').load('listados/tabla_invitados.php');
 });
+
+function agregarInvitado(){
+    $.ajax({
+        type:"POST",
+        data:$('#frmAgregarInvitado').serialize(),
+        url:"../servidor/invitados/agregar.php",
+        success:function(respuesta){
+            if(respuesta == 1){
+                $('#tablaInvitados').load('listados/tabla_invitados.php');
+                $('#frmAgregarInvitado')[0].reset();
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Invitado Agregado con Exito',
+                    showConfirmButton: false,
+                    timer: 1700
+                  })
+            }else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'El Invitado no se a Podido Agregr' + respuesta,
+                    showConfirmButton: false,
+                    timer: 1700
+                  })
+            }
+        }
+    });
+}
