619b60b94b9b77f88ad0962f29c93034ea310ef6
BryanCachimuel <bryanloyo56@gmail.com>
2023-05-31 21:48:08 -0500

    estructurando el formulario para hacer el ingreso de registros para los invitados

--- modulos/listados/modal_agregar.php
+++ modulos/listados/modal_agregar.php
@@ -1,18 +1,30 @@
-<!-- Modal -->
-<div class="modal fade" id="modal_agregar_invitado" tabindex="-1" aria-labelledby="modal_agregar_invitadoLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modal_agregar_invitadoLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+<?php
+  include "../clases/Invitados.php";
+  $Invitados = new Invitados();
+  $select =$Invitados->seleccionarEventos();
+?>
+
+<form id="frmAgregarInvitado" onsubmit="return agregarInvitado()">
+    <!-- Modal -->
+  <div class="modal fade" id="modal_agregar_invitado" tabindex="-1" aria-labelledby="modal_agregar_invitadoLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="modal_agregar_invitadoLabel">Agregar Invitados</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <?php echo $select; ?>
+          <label for="nombre_invitado">Nombre Invitado: </label>
+          <input type="text" class="form-control" id="nombre_invitado" name="nombre_invitado">
+          <label for="email_invitado">Email Invitado: </label>
+          <input type="email" class="form-control" id="email_invitado" name="email_invitado">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button class="btn btn-primary">Guardar</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
+</form>
