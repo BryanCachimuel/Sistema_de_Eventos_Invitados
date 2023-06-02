c5a167431525c61d8226ed50fea7427adb78a94c
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-01 20:51:59 -0500

    determinado en el boton eliminar el id_invitado para poder eliminar el invitado seleccionado de la tabla

--- modulos/listados/tabla_invitados.php
+++ modulos/listados/tabla_invitados.php
@@ -27,7 +27,7 @@
                 </span>
             </td>
             <td>
-                <span class="btn btn-danger">
+                <span class="btn btn-danger" onclick="eliminarInvitado('<?php echo $inv['idInvitado'] ?>')">
                     <i class="fa-solid fa-user-xmark"></i>
                 </span>
             </td>
