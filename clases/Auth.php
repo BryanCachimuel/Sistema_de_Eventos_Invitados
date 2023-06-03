e1ca7c5b2b2669618afb2645870741efb848482f
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-02 21:44:09 -0500

    creando la función para listar los usuarios

--- clases/Auth.php
+++ clases/Auth.php
@@ -44,6 +44,17 @@
            }
         }
 
+        public function listarUsuarios(){
+            try {
+                $conexion = parent::conectar();
+                $sql = "SELECT * FROM usuarios";
+                $respuesta = mysqli_query($conexion,$sql);
+                return mysqli_fetch_all($respuesta, MYSQLI_ASSOC);
+            } catch (Exception $e) {
+                echo "No se a podido mostrar el listado de usuarios: ".$e;
+            }
+        }
+
     }
 
 ?>
