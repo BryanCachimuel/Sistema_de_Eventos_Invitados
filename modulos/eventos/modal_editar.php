828d393e95be4ca11512b8dd8cd7ef50be6374d3
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-05 20:15:33 -0500

    dando nombres a los identificadores de cada input del formulario y agregando un input de tipo hidden para el id_evento

--- modulos/eventos/modal_editar.php
+++ modulos/eventos/modal_editar.php
@@ -1,4 +1,4 @@
-<form id="frmEditarEvento" onsubmit="return editarEvento()">
+<form id="frmEditarEvento" onsubmit="return actualizarEvento()">
     <!-- Modal -->
     <div class="modal fade" id="modal_editar_evento" tabindex="-1" aria-labelledby="modal_editar_eventoLabel" aria-hidden="true">
     <div class="modal-dialog">
@@ -8,18 +8,19 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <label for="evento_nombre">Nombre del Evento: </label>
-          <input type="text" class="form-control" id="evento_nombre" name="evento_nombre" required>
-          <label for="hora_inicio">Hora Inicio: </label>
-          <input type="time" class="form-control" id="hora_inicio" name="hora_inicio" required>
-          <label for="hora_fin">Hora Fin: </label>
-          <input type="time" class="form-control" id="hora_fin" name="hora_fin" required>
-          <label for="fecha">Fecha: </label>
-          <input type="date" class="form-control" id="fecha" name="fecha" required>
+          <input type="hidden" name="id_evento" id="id_evento">
+          <label for="evento_nombreu">Nombre del Evento: </label>
+          <input type="text" class="form-control" id="evento_nombreu" name="evento_nombreu" required>
+          <label for="hora_iniciou">Hora Inicio: </label>
+          <input type="time" class="form-control" id="hora_iniciou" name="hora_iniciou" required>
+          <label for="hora_finu">Hora Fin: </label>
+          <input type="time" class="form-control" id="hora_finu" name="hora_finu" required>
+          <label for="fechau">Fecha: </label>
+          <input type="date" class="form-control" id="fechau" name="fechau" required>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-          <button class="btn btn-primary">Editar</button>
+          <button class="btn btn-primary">Actualizar</button>
         </div>
       </div>
     </div>
