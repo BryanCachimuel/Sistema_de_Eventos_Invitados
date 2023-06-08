90d69afcfd2942036941f0b19a15bcc249dc8115
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-07 22:16:26 -0500

    agregando iconos y espacios para el modal de agregar invitados

--- modulos/listados/modal_agregar.php
+++ modulos/listados/modal_agregar.php
@@ -1,6 +1,4 @@
 <?php
-  include "../clases/Invitados.php";
-  $Invitados = new Invitados();
   $select =$Invitados->seleccionarEventos();
 ?>
 
@@ -9,20 +7,28 @@
   <div class="modal fade" id="modal_agregar_invitado" tabindex="-1" aria-labelledby="modal_agregar_invitadoLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="modal_agregar_invitadoLabel">Agregar Invitados</h1>
+        <div class="modal-header bg-primary">
+          <h1 class="modal-title fs-5" id="modal_agregar_invitadoLabel"><i class="fa-solid fa-circle-check"></i> Agregar Invitados</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-          <?php echo $select; ?>
-          <label for="nombre_invitado">Nombre Invitado: </label>
-          <input type="text" class="form-control" id="nombre_invitado" name="nombre_invitado" required>
-          <label for="email_invitado">Email Invitado: </label>
-          <input type="email" class="form-control" id="email_invitado" name="email_invitado" required>
+          <div class="mb-3">
+            <?php echo $select; ?>
+          </div>
+
+          <div class="mb-3">
+            <label for="nombre_invitado">Nombre Invitado: </label>
+            <input type="text" class="form-control" id="nombre_invitado" name="nombre_invitado" required>
+          </div>
+          
+          <div>
+            <label for="email_invitado">Email Invitado: </label>
+            <input type="email" class="form-control" id="email_invitado" name="email_invitado" required>
+          </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-          <button class="btn btn-primary">Guardar</button>
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i class="fa-solid fa-xmark"></i> Cancelar</button>
+          <button class="btn btn-primary"><i class="fa-regular fa-floppy-disk"></i> Guardar</button>
         </div>
       </div>
     </div>
