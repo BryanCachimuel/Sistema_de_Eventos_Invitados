5064c99402b8724fc1e00d07bde7e9bdbbf58c1d
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-07 22:17:51 -0500

    agregando iconos y espacios entre los inputs en el modal de editar invitados

--- modulos/listados/modal_editar.php
+++ modulos/listados/modal_editar.php
@@ -6,21 +6,29 @@
   <div class="modal fade" id="modal_editar_invitado" tabindex="-1" aria-labelledby="modal_editar_invitadoLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="modal_editar_invitadoLabel">Editar Invitado</h1>
+        <div class="modal-header bg-primary">
+          <h1 class="modal-title fs-5" id="modal_editar_invitadoLabel"><i class="fa-solid fa-file-pen"></i> Editar Invitado</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
           <input type="hidden" name="id_invitado" id="id_invitado">
-          <?php echo $seleccionar; ?>
-          <label for="nombre_invitadou">Nombre Invitado: </label>
-          <input type="text" class="form-control" id="nombre_invitadou" name="nombre_invitadou" required>
-          <label for="email_invitadou">Email Invitado: </label>
-          <input type="email" class="form-control" id="email_invitadou" name="email_invitadou" required>
+          <div class="mb-3">
+            <?php echo $seleccionar; ?>
+          </div>
+         
+          <div class="mb-3">
+            <label for="nombre_invitadou">Nombre Invitado: </label>
+            <input type="text" class="form-control" id="nombre_invitadou" name="nombre_invitadou" required>
+          </div>
+          
+          <div class="mb-3">
+            <label for="email_invitadou">Email Invitado: </label>
+            <input type="email" class="form-control" id="email_invitadou" name="email_invitadou" required>
+          </div>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-          <button class="btn btn-primary">Actualizar</button>
+          <button type="button" class="btn btn-danger" data-bs-dismiss="modal"><i class="fa-solid fa-xmark"></i> Cancelar</button>
+          <button class="btn btn-primary"><i class="fa-solid fa-pen-nib"></i> Actualizar</button>
         </div>
       </div>
     </div>
