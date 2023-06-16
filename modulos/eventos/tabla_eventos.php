bc6748252e939092efd9ef271a00a3706a948f46
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-15 19:28:01 -0500

    agregando las validaciones necesarias dentro de la tabla de eventos para poder mostrar por pantalla la cantidad de invitados por evento

--- modulos/eventos/tabla_eventos.php
+++ modulos/eventos/tabla_eventos.php
@@ -1,8 +1,8 @@
 <?php
-    session_start();
-    include "../../clases/Eventos.php";
-    $Eventos = new Eventos();
-    $items =$Eventos->mostrarEventos($_SESSION['id_usuario']);
+session_start();
+include "../../clases/Eventos.php";
+$Eventos = new Eventos();
+$items = $Eventos->mostrarEventos($_SESSION['id_usuario']);
 ?>
 <table class="table table-sm table-hover" id="tabla_eventos_load">
     <thead>
@@ -11,63 +11,77 @@
             <th>Hora Inicio</th>
             <th>Hora Fin</th>
             <th>Fecha</th>
+            <th>Cantidad Invitados</th>
+            <th>Imprimir</th>
             <th>Editar</th>
             <th>Eliminar</th>
-            <th>Impremir</th>
         </tr>
     </thead>
     <tbody>
-        <?php foreach($items as $evt) : ?>
-        <tr>
-            <td><?php echo $evt['evento_nombre']?></td>
-            <td><?php echo $evt['hora_inicio'] ?></td>
-            <td><?php echo $evt['hora_fin'] ?></td>
-            <td><?php echo $evt['fecha'] ?></td>
-            <td>
-                <span class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modal_editar_evento" onclick="editarEvento('<?php echo $evt['id_evento']?>')">
-                    <i class="fa-solid fa-pen-to-square"></i>
-                </span>
-            </td>
-            <td>
-                <span class="btn btn-danger" onclick="eliminarEvento('<?php echo $evt['id_evento'] ?>')">
-                    <i class="fa-solid fa-trash-can"></i>
-                </span>
-            </td>
-            <td>
-                <a href="../servidor/impresion_evento/imprimir_evento.php?id_evento=<?php echo $evt['id_evento'] ?>" class="btn btn-info" target="_blank">
-                    <i class="fa-solid fa-print"></i>
-                </a>
-            </td>
-        </tr>
+        <?php foreach ($items as $evt) : ?>
+            <tr>
+                <td><?php echo $evt['evento_nombre'] ?></td>
+                <td><?php echo $evt['hora_inicio'] ?></td>
+                <td><?php echo $evt['hora_fin'] ?></td>
+                <td><?php echo $evt['fecha'] ?></td>
+                <td>
+                    <?php $invitadosExistentes = $Eventos->existenInvitados($evt['id_evento']);  ?>
+                    <?php echo $invitadosExistentes ?>
+                </td>
+                <td>
+                    <?php
+                    $invitadosExistentes = $Eventos->existenInvitados($evt['id_evento']);
+                    if ($invitadosExistentes > 0) {
+                    ?>
+                        <a href="../servidor/impresion_evento/imprimir_evento.php?id_evento=<?php echo $evt['id_evento'] ?>" class="btn btn-info" target="_blank">
+                            <i class="fa-solid fa-print"></i>
+                        </a>
+                    <?php
+                    }
+                    ?>
+                </td>
+                <td>
+                    <span class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modal_editar_evento" onclick="editarEvento('<?php echo $evt['id_evento'] ?>')">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </span>
+                </td>
+                <td>
+                    <span class="btn btn-danger" onclick="eliminarEvento('<?php echo $evt['id_evento'] ?>')">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </span>
+                </td>
+            </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         $('#tabla_eventos_load').DataTable({
-            "language":{
-              "decimal": "",
-              "emptyTable": "No hay información",
-              "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-              "infoEmpty": "Mostrando 0 a 0 de 0 Entradas",
-              "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-              "infoPostFix": "",
-              "thousands": ",",
-              "lengthMenu": "Mostrar _MENU_ Entradas",
-              "loadingRecords": "Cargando...",
-              "processing": "Procesando...",
-              "search": "Buscar:",
-              "zeroRecords": "Sin resultados encontrados",
-              "paginate": {
-                  "first": "Primero",
-                  "last": "Ultimo",
-                  "next": "Siguiente",
-                  "previous": "Anterior"
-              }
-          },
-          "lengthMenu": [[5, 10, 15, 20], [5, 10, 15, "All"]
-          ],
-      });
+            "language": {
+                "decimal": "",
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 a 0 de 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+            "lengthMenu": [
+                [5, 10, 15, 20],
+                [5, 10, 15, "All"]
+            ],
+        });
     });
 </script>
