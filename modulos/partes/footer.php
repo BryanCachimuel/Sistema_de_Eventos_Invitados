7032c74c73d8c0ee07740df65f4b3d66a45f722c
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-19 19:42:10 -0500

    insertando el cdn para fullcalendar y estilos para el calendario que se encuentra en la vista de inicio del sistema

--- modulos/partes/footer.php
+++ modulos/partes/footer.php
@@ -2,5 +2,6 @@
     <script src="../publico/librerias/datatables/datatables.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
+    <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.5/index.global.min.js'></script>
   </body>
 </html>
