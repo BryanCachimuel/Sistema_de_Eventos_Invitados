54531be8b8174abc349ae619a38f8b72e54942db
BryanCachimuel <bryanloyo56@gmail.com>
2023-06-21 19:15:03 -0500

    agregando un input de tipo buscador para buscar en la tabla eventos por fecha

--- modulos/eventos.php
+++ modulos/eventos.php
@@ -1,6 +1,6 @@
-<?php 
-  include "partes/header.php"; 
-  include "partes/menu.php";
+<?php
+include "partes/header.php";
+include "partes/menu.php";
 ?>
 
 <!-- Contenido -->
@@ -10,9 +10,27 @@
       <div class="card mt-3">
         <div class="card-body">
           <h3 class="titulo">Listado de Eventos</h3> <br>
-          <span href="" class="btn btn-purple" data-bs-toggle="modal" data-bs-target="#modal_agregar_evento">
-            <i class="fa-solid fa-circle-plus"></i> Evento Nuevo
-          </span>
+
+
+          <div class="row">
+            <div class="col">
+              <span href="" class="btn btn-purple" data-bs-toggle="modal" data-bs-target="#modal_agregar_evento">
+                <i class="fa-solid fa-circle-plus"></i> Evento Nuevo
+              </span>
+            </div>
+
+            <div class="col"></div>
+
+            <div class="col">
+              <div class="input-group mb-3">
+                <input type="date" class="form-control" id="fechaBuscar">
+                <span class="btn btn-purple" onclick="buscarPorFecha()">
+                  <i class="fa-solid fa-magnifying-glass"></i> Buscar
+                </span>
+              </div>
+            </div>
+          </div>
+
           <hr>
           <div id="tablaEventos"></div>
         </div>
@@ -22,13 +40,10 @@
 </div>
 
 
-<?php 
-  include "eventos/modal_agregar.php";
-  include "eventos/modal_editar.php";
-  include "partes/footer.php"; 
+<?php
+include "eventos/modal_agregar.php";
+include "eventos/modal_editar.php";
+include "partes/footer.php";
 ?>
 
 <script src="../publico/js/eventos.js"></script>
-
-
-    
