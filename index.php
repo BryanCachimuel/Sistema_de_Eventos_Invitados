c3356225c2135ff4a7da726c51231b1fde4dee4e
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-02 21:36:48 -0500

    maquetando la vista del index

--- index.php
+++ index.php
@@ -5,9 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sistema de Eventos e Invitados</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="publico/css/index.css">
   </head>
   <body>
-    <a class="btn btn-primary" href="inicio_sesion.php">Inicio de Sesión</a>
+  <header class="hero">
+        <nav class="nav container"></nav>
+
+        <section class="hero__container container">
+            <h1 class="hero__title">Sistema de Eventos e Invitados</h1>
+            <p class="hero__paragraph">
+              Bienvenidos al Sistema donde podras agendar tu respectivo evento y hacer la reserva para 
+              todos los invitados que desees
+            </p>
+               
+            <a href="inicio_sesion.php" class="cta">Iniciar Sesión</a>
+        </section>
+    </header>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
 </html>
