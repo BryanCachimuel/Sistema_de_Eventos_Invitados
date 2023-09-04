1945c5375f3edc5bdc4f1388a5fe096777ffa5fb
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-03 22:07:02 -0500

    creando la función editarUsuario por id_usuario para poder seleccionar un usuario para su id

--- clases/Usuarios.php
+++ clases/Usuarios.php
@@ -15,6 +15,18 @@ class Usuarios extends Conexion {
         }
     }
 
+    public function editarUsuarios($id_usuario){
+        try {
+            $conexion = parent::conectar();
+            $sql = "SELECT * FROM usuarios
+                    WHERE id_usuario='$id_usuario'";
+            $respuesta = mysqli_query($conexion,$sql);
+            return mysqli_fetch_all($respuesta,MYSQLI_ASSOC);
+        } catch (Exception $e) {
+            echo "No se a podido mostrar los datos a editar en el formulario: ".$e;
+        }
+    }
+
 }
 
 ?>
