23f724bccc07b04ad9d1f1c9e9effa4f53f1b9cd
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-03 22:13:42 -0500

    estableciendo en el archivo usuarios.js una función para hacer la edición de un usuario en donde se realiza mediante ajax un proceso de obtención de los datos que se veran reflejados en el modal de edición de usuarios

--- publico/js/usuarios.js
+++ publico/js/usuarios.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
     $("#tablaUsuarios").load("usuarios/tabla_usuarios.php");
   });
+
+function editarUsuarios(id_usuario){
+  $.ajax({
+    type:"POST",
+    url:"../servidor/usuarios/editar.php",
+    data:"id_usuario=" + id_usuario,
+    success:function(respuesta){
+      respuesta = jQuery.parseJSON(respuesta);
+        $('#usuario_nombreu').val(respuesta[0].usuario);
+        $('#usuario_correou').val(respuesta[0].correo);
+    }
+  });
+}
