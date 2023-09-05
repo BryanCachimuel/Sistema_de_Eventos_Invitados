84c581451b9a11aec30a6c65590deae22b7033d9
BryanCachimuel <bryanloyo56@gmail.com>
2023-09-04 22:16:23 -0500

    creando el método para actualizar un usuario de acuerdo a los datos que se proporcionen en la base de datos

--- clases/Usuarios.php
+++ clases/Usuarios.php
@@ -18,12 +18,28 @@ class Usuarios extends Conexion {
     public function editarUsuarios($id_usuario){
         try {
             $conexion = parent::conectar();
-            $sql = "SELECT * FROM usuarios
+            $sql = "SELECT usuario,correo FROM usuarios
                     WHERE id_usuario='$id_usuario'";
             $respuesta = mysqli_query($conexion,$sql);
             return mysqli_fetch_all($respuesta,MYSQLI_ASSOC);
         } catch (Exception $e) {
             echo "No se a podido mostrar los datos a editar en el formulario: ".$e;
+        }
+    }
+
+    public function actualizarUsuario($data){
+        try {
+            $conexion = parent::conectar();
+            $sql = "UPDATE usuarios SET usuario=?,
+                                        correo=?
+                    WHERE id_usuario=?";
+            $query = $conexion->prepare($sql);
+            $query->bind_param('ssi',$data['usuario'],
+                                      $data['correo'],
+                                      $data['id_usuario']);
+            return $query->execute();
+        } catch (Exception $e) {
+            echo "No se a podido actualizar el usuario: ".$e;
         }
     }
 
